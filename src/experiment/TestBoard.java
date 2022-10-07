5d116b56782c9fde01737d86d1d04aed8259dc86
FloorismadeoutofFloor <gwdina@gmail.com>
2022-10-07 13:53:29 -0600

    added public to functions and started on BoardTestsExp

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -9,20 +9,22 @@ public class TestBoard {
 
 	}
 	
-	//void calcTargets( TestBoardCell startCell, int pathlength) – calculates legal targets for a move from startCell of length pathlength.
+	//void calcTargets( TestBoardCell startCell, int pathlength) ï¿½ calculates legal targets for a move from startCell of length pathlength.
 	public void calcTargets( TestBoardCell startCell, int pathlength) {
 		
 	}
 	
-	//TestBoardCell getCell( int row, int col ) – returns the cell from the board at row, col.
+	//TestBoardCell getCell( int row, int col ) ï¿½ returns the cell from the board at row, col.
 	
-	TestBoardCell getCell( int row, int col ) {
+	public TestBoardCell getCell( int row, int col ) {
 		return null;
+		
 		
 	}
 	
-	//Set<TestBoardCell> getTargets() – gets the targets last created by calcTargets()
-	Set<TestBoardCell> getTargets() {
+	//Set<TestBoardCell> getTargets() ï¿½ gets the targets last created by calcTargets()
+	public Set<TestBoardCell> getTargets() {
+		
 		return null;
 		
 	}
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,9 +1,21 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import experiment.*;
+
 public class BoardTestsExp {
+	TestBoard board;
 	
 	//@BeforeEach method to set up your BoardExp.
+	public void setUp() {
+		board = new TestBoard();
+	}
 	
-	
-
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0,0);
+	}
 }
