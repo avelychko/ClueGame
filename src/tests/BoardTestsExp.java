11fd8a861b50fe91dda34f4bf216168902002fb1
FloorismadeoutofFloor <gwdina@gmail.com>
2022-10-07 13:58:15 -0600

    cleaned up boardtests

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -8,17 +8,7 @@ import experiment.*;
 
 public class BoardTestsExp {
 	TestBoard board;
-	
-<<<<<<< HEAD
-	public void testBoard() {
-		
-	}
-	
-	public void testBoardCell() {
-		
-	}
 
-=======
 	//@BeforeEach method to set up your BoardExp.
 	public void setUp() {
 		board = new TestBoard();
@@ -28,5 +18,5 @@ public class BoardTestsExp {
 	public void testAdjacency() {
 		TestBoardCell cell = board.getCell(0,0);
 	}
->>>>>>> 5d116b56782c9fde01737d86d1d04aed8259dc86
+
 }
