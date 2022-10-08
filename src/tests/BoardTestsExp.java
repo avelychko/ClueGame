ece576b5c1e9f46dd6c95dfe90c4eb9c4611bb18
FloorismadeoutofFloor <gwdina@gmail.com>
2022-10-07 23:40:21 -0600

    add testTargetsNormal anad TargetsMixed

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -73,17 +73,32 @@ public class BoardTestsExp {
 	public void testTargetsNormal() {
 	TestBoardCell cell = board.getCell(0,0);
 		board.calcTargets(cell, 3);
-		
 		Set<TestBoardCell> target = board.getTargets();
+		Assert.assertEquals(6, target.size());
+		Assert.assertTrue(target.contains(board.getCell(3, 0)));
+		Assert.assertTrue(target.contains(board.getCell(2, 1)));
+		Assert.assertTrue(target.contains(board.getCell(0, 1)));
+		Assert.assertTrue(target.contains(board.getCell(1, 2)));
+		Assert.assertTrue(target.contains(board.getCell(0, 3)));
+		Assert.assertTrue(target.contains(board.getCell(1, 0)));
 		
-		assertEquals(6, target.size());
-		assertTrue(target.contains(board.getCell(3, 0)));
-		assertTrue(target.contains(board.getCell(2, 1)));
-		assertTrue(target.contains(board.getCell(0, 1)));
-		assertTrue(target.contains(board.getCell(1, 2)));
-		assertTrue(target.contains(board.getCell(0, 3)));
-		assertTrue(target.contains(board.getCell(1, 0)));
-		
+	}
+	
+	/*
+	 * 
+	 */
+	@Test
+	public void testTargetsMixed() {
+		//set up occupied cells
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2).setRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
 
 }
