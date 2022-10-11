1aec79c7602fe3d34e880430bfec61fcdc5dec13
Anastasia Velychko <avelychko@mines.edu>
2022-10-11 13:26:56 -0600

    cleane up, failed ExceptionTests

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -2,12 +2,8 @@ package clueGame;
 
 import java.util.*;
 
-//import experiment.TestBoardCell;
-
 public class Board {
 	private BoardCell[][] grid; //2D array of object TestBoardCell
-//	private Set<BoardCell> targets; //set of target cells
-//	private Set<BoardCell> visited; //set of visited cells
 	private int numColumns; //column size
 	private int numRows; //row size
 	private String layoutConfigFile; 
@@ -30,29 +26,7 @@ public class Board {
      /*
       * initialize the board (since we are using singleton pattern)
       */
-     public void initialize()
-     {
- 		grid = new BoardCell[numRows][numColumns]; //set grid at size ROWS and COLS
-// 		targets = new HashSet<BoardCell>(); //initialize targets
-// 		visited = new HashSet<BoardCell>(); //initialize visited
-
- 		//for loop that creates grid at rows and cols
- 		for (int i = 0; i < numRows; i++) {
- 			for (int j = 0; j < numColumns; j++) {
- 				grid[i][j] = new BoardCell(i, j);
- 			}
- 		}
-
- 		//adds 4 adjacencies of rows and cols
- 		for (int i = 0; i < numRows; i++) {
- 			for (int j = 0; j < numColumns; j++) {
- 				if ((i-1) >= 0) grid[i][j].addAdj(getCell(i-1, j)); //adjacency in x-1 direction
- 				if ((j-1) >= 0) grid[i][j].addAdj(getCell(i, j-1)); //adjacency in y-1 direction
- 				if ((i+1) < numRows) grid[i][j].addAdj(getCell(i+1, j)); //adjacency in x+1 direction
- 				if ((j+1) < numColumns) grid[i][j].addAdj(getCell(i, j+1)); //adjacency in y+1 direction
- 			}
- 		}
-     }
+     public void initialize() { grid = new BoardCell[numRows][numColumns]; } //set grid size
      
      public void loadSetupConfig() {
     	 
@@ -61,33 +35,12 @@ public class Board {
      public void loadLayoutConfig() {
     	 
      }
-
-//	//calculates legal targets for a move from startCell of length pathlength
-//	public void calcTargets(BoardCell startCell, int pathlength) {
-//		visited.add(startCell); //adds visited cell to visited set
-//		//for loop for each adjacency cell
-//		for (BoardCell adjCell: startCell.adjList) {
-//			//checks if adjacency cell has already been visited	
-//			if (visited.contains(adjCell) == false) {
-//				//checks if cell is occupied
-//				if (adjCell.getOccupied() == false) {
-//					if (pathlength == 1) targets.add(adjCell); //checks if length is 1 then add adj cell to targets set
-//					if (adjCell.getRoom() == true) targets.add(adjCell); //if is room add adj cell to targets
-//					else calcTargets(adjCell, pathlength - 1);  //else call adj cell recursively
-//					visited.remove(adjCell); //remove visited adj cell
-//				}
-//			}
-//		}
-//	}
+     
+ 	public void setConfigFiles(String layout, String setup) {
+		this.layoutConfigFile = layout;
+		this.setupConfigFile = setup;
+	}
 
 	//returns the cell from the board at row, col
-	public BoardCell getCell(int row, int col) {
-		return grid[row][col];
-	}
-
-	//gets the targets last created by calcTargets()
-	public Set<BoardCell> getTargets() {
-		return targets;
-	}
-
+	public BoardCell getCell(int row, int col) { return grid[row][col]; }
 }
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,9 +1,6 @@
 package clueGame;
 
-import java.util.HashSet;
-import java.util.Set;
-
-//import experiment.TestBoardCell;
+import java.util.*;
 
 public class BoardCell {
 	private int row, col; //row and col variables
@@ -12,8 +9,6 @@ public class BoardCell {
 	private boolean roomLabel;
 	private boolean roomCenter;
 	private char SecretPassage;
-//	private Boolean isRoom = false; //isRoom boolean initialized to false
-//	private Boolean isOccupied = false; //isOccupied initialized to false
 	Set<BoardCell> adjList = new HashSet<BoardCell>(); //initialized adjList set
 	
 	//constructor
@@ -34,25 +29,4 @@ public class BoardCell {
 	public Set<BoardCell> getAdjList() {
 		return adjList;
 	}
-
-//	//a setter for indicating a cell is part of a room 
-//	public void setRoom(boolean bool) {
-//		this.isRoom = bool;
-//	}
-//	
-//	//a getter for indicating a cell is part of a room 
-//	public boolean getRoom() {
-//		return this.isRoom;
-//	}
-//
-//	//a setter for indicating a cell is occupied by another player
-//	public void setOccupied(boolean bool) {
-//		this.isOccupied = bool;
-//	}
-//	
-//	//a getter for indicating a cell is occupied by another player
-//	public boolean getOccupied() {
-//		return this.isOccupied;
-//	}
-
 }
