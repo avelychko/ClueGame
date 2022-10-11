700a3668476762415ca3b5a7cc38e272fb4ee0e5
FloorismadeoutofFloor <gwdina@gmail.com>
2022-10-11 12:48:21 -0600

    copy and pasted things from test to cluegame

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,67 @@
 package clueGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
+//import experiment.TestBoardCell;
+
 public class Board {
+	private BoardCell[][] grid; //2D array of object TestBoardCell
+	private Set<BoardCell> targets; //set of target cells
+	private Set<BoardCell> visited; //set of visited cells
+	final static int COLS = 4; //column size
+	final static int ROWS = 4; //row size
+
+	//constructor
+	public Board() {
+		grid = new BoardCell[ROWS][COLS]; //set grid at size ROWS and COLS
+		targets = new HashSet<BoardCell>(); //initialize targets
+		visited = new HashSet<BoardCell>(); //initialize visited
+
+		//for loop that creates grid at rows and cols
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				grid[i][j] = new BoardCell(i, j);
+			}
+		}
+
+		//adds 4 adjacencies of rows and cols
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				if ((i-1) >= 0) grid[i][j].addAdjacency(getCell(i-1, j)); //adjacency in x-1 direction
+				if ((j-1) >= 0) grid[i][j].addAdjacency(getCell(i, j-1)); //adjacency in y-1 direction
+				if ((i+1) < ROWS) grid[i][j].addAdjacency(getCell(i+1, j)); //adjacency in x+1 direction
+				if ((j+1) < COLS) grid[i][j].addAdjacency(getCell(i, j+1)); //adjacency in y+1 direction
+			}
+		}
+	}
+
+	//calculates legal targets for a move from startCell of length pathlength
+	public void calcTargets(BoardCell startCell, int pathlength) {
+		visited.add(startCell); //adds visited cell to visited set
+		//for loop for each adjacency cell
+		for (BoardCell adjCell: startCell.adjList) {
+			//checks if adjacency cell has already been visited	
+			if (visited.contains(adjCell) == false) {
+				//checks if cell is occupied
+				if (adjCell.getOccupied() == false) {
+					if (pathlength == 1) targets.add(adjCell); //checks if length is 1 then add adj cell to targets set
+					if (adjCell.getRoom() == true) targets.add(adjCell); //if is room add adj cell to targets
+					else calcTargets(adjCell, pathlength - 1);  //else call adj cell recursively
+					visited.remove(adjCell); //remove visited adj cell
+				}
+			}
+		}
+	}
+
+	//returns the cell from the board at row, col
+	public BoardCell getCell(int row, int col) {
+		return grid[row][col];
+	}
+
+	//gets the targets last created by calcTargets()
+	public Set<BoardCell> getTargets() {
+		return targets;
+	}
 
 }
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,53 @@
 package clueGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import experiment.TestBoardCell;
+
 public class BoardCell {
+	private int row, col; //row and col variables
+	private Boolean isRoom = false; //isRoom boolean initialized to false
+	private Boolean isOccupied = false; //isOccupied initialized to false
+	Set<BoardCell> adjList = new HashSet<BoardCell>(); //initialized adjList set
+	
+	//constructor
+	public BoardCell() { adjList = new HashSet<BoardCell>(); }
+	
+	//a constructor that has passed into it the row and column for that cell
+	public BoardCell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	//a setter to add a cell to this cells adjacency list
+	public void addAdjacency(BoardCell cell) {
+		this.adjList.add(cell);
+	}
+
+	//returns the adjacency list for the cell
+	public Set<BoardCell> getAdjList() {
+		return adjList;
+	}
+
+	//a setter for indicating a cell is part of a room 
+	public void setRoom(boolean bool) {
+		this.isRoom = bool;
+	}
+	
+	//a getter for indicating a cell is part of a room 
+	public boolean getRoom() {
+		return this.isRoom;
+	}
+
+	//a setter for indicating a cell is occupied by another player
+	public void setOccupied(boolean bool) {
+		this.isOccupied = bool;
+	}
+	
+	//a getter for indicating a cell is occupied by another player
+	public boolean getOccupied() {
+		return this.isOccupied;
+	}
 
 }
