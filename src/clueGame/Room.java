0ea7833d147f998acd6bcb93a580ab1dad673e77
FloorismadeoutofFloor <gwdina@gmail.com>
2022-10-11 13:27:32 -0600

    put basics for Room class

--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -1,5 +1,16 @@
 package clueGame;
 
 public class Room {
+	private String name;
+	private BoardCell centerCell;
+	private BoardCell labelCell;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
 
 }
