4aa9e39efc48f44ace7515ce4672f81ee0ef7a07
Anastasia Velychko <avelychko@mines.edu>
2022-10-12 02:42:10 -0600

    worked on loading setup

--- src/testing.java
+++ src/testing.java
@@ -11,16 +11,6 @@ public class testing {
 		Scanner in = new Scanner(reader);
 		String line;
 		in.useDelimiter(", ");
-
-//		while (in.hasNext()) {
-//			line = in.next();
-//			if (!line.contains("//")) {
-//				System.out.println(line);
-//				line = Character.toString(in.next().charAt(0));
-//				System.out.println(line);
-//				in.next();
-//			}
-//		} in.close();
 		
  		while (in.hasNext()) {
  			line = in.next();
@@ -28,7 +18,7 @@ public class testing {
  				Room room = new Room();
 				room.setName(line);
 				line = in.next();
- 				roomMap.put(line.charAt(0), room);
+ 				roomMap.put(line.charAt(0), );
  			}
  		} in.close();
 		System.out.println(roomMap);
